ac5235a7881090c01e4dfdcf75d2dc572cb34539
sagarsingh045 <sagarsingh831933@gmail.com>
2026-02-09 17:44:25 +0000

    config done

---
+++ JAVA/_variables.js
@@ -0,0 +1,13 @@
+
+
+// There are three type of variables in js.
+let x = 10
+console.log(x);
+// let  used assign value to a variable normally we are using let.
+var y = 11
+console.log(y)
+//var used assign value to a variable but in var the scope is a factor.
+const z = 20
+console.log(z);
+//const used for which value can not be changed in whole code it remains constant.
+
